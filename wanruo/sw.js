e37d92ef77aaf63447e3f1385f533b11493526c3
chenshimeng <1285348378@qq.com>
2026-07-08 13:08:18 +0800

    fix: PWA缓存更新和保存状态

--- wanruo/sw.js
+++ wanruo/sw.js
@@ -1,5 +1,12 @@
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => self.clients.claim());
+const CACHE = 'metronome-v2';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/metronome/'])));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
 self.addEventListener('fetch', e => {
   e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
